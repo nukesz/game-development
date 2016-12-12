78021998be47f00c6dff212141901bfd511fe200
Norbert Benczúr <norbert.benczur@gmail.com>
2016-12-12 17:12:15 +0100

    Created a screen for the game

---
+++ snake/core/src/com/nukesz/game/GameScreen.java
@@ -0,0 +1,27 @@
+package com.nukesz.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.awt.*;
+
+public class GameScreen extends ScreenAdapter {
+
+    private SpriteBatch batch;
+
+    @Override
+    public void show() {
+        batch = new SpriteBatch();
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(Color.BLACK.getRed(), Color.BLACK.getGreen(), Color.BLACK.getBlue(), Color.BLACK.getAlpha());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.end();
+    }
+}
