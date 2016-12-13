1f8c49dd4e3370b77ef52073bdba62aae20ba358
Norbert Benczúr <norbert.benczur@gmail.com>
2016-12-13 17:11:38 +0100

    Started to add body to the snake

---
+++ snake/core/src/com/nukesz/game/BodyPart.java
@@ -0,0 +1,23 @@
+package com.nukesz.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+
+public class BodyPart {
+    private int x, y;
+    private Texture texture;
+
+    public BodyPart(Texture texture) {
+        this.texture = texture;
+    }
+
+    public void updateBodyPosition(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void draw(Batch batch, int snakeX, int snakeY) {
+        if (!(x == snakeX && y == snakeY)) batch.draw(texture, x, y);
+    }
+
+}
--- snake/core/src/com/nukesz/game/GameScreen.java
+++ snake/core/src/com/nukesz/game/GameScreen.java
@@ -1,10 +1,13 @@
 package com.nukesz.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.Array;
 
 import java.awt.*;
 
@@ -16,7 +19,7 @@ public class GameScreen extends ScreenAdapter {
     private static final int DOWN = 3;
     private int snakeDirection = UP;
 
-    private static final float MOVE_TIME = 1F;
+    private static final float MOVE_TIME = 0.2F;
     private static final int SNAKE_MOVEMENT = 32;
     private float timer = MOVE_TIME;
     private int snakeX = 0;
@@ -24,27 +27,66 @@ public class GameScreen extends ScreenAdapter {
 
     private SpriteBatch batch;
     private Texture snakeHead;
+    private Texture snakeBody;
+    private Texture apple;
+    private boolean appleAvailable = false;
+    private int appleX;
+    private int appleY;
+    private Array<BodyPart> bodyParts = new Array<BodyPart>();
 
     @Override
     public void show() {
         batch = new SpriteBatch();
         snakeHead = new Texture(Gdx.files.internal("snakehead_resized.png"));
+        snakeBody = new Texture(Gdx.files.internal("snakebody.png"));
+        apple = new Texture(Gdx.files.internal("apple.png"));
     }
 
     @Override
     public void render(float delta) {
+        queryInput();
         timer -= delta;
         if (timer <= 0) {
             timer = MOVE_TIME;
             moveSnake();
+            checkForOutOfBounds();
         }
-        checkForOutOfBounds();
+        checkAppleCollision();
+        checkAndPlaceApple();
+        clearScreen();
+        batch.begin();
+        draw();
+    }
+
+    private void draw() {
+        batch.draw(snakeHead, snakeX, snakeY);
+        if (appleAvailable) {
+            batch.draw(apple, appleX, appleY);
+        }
+        batch.end();
+    }
+
+    private void clearScreen() {
         Gdx.gl.glClearColor(Color.BLACK.getRed(), Color.BLACK.getGreen(), Color.BLACK.getBlue(), Color.BLACK.getAlpha());
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.draw(snakeHead, snakeX, snakeY);
-        batch.end();
+    }
 
+    private void checkAppleCollision() {
+        if (appleAvailable && appleX == snakeX && appleY == snakeY) {
+            appleAvailable = false;
+        }
+    }
+
+    private void checkAndPlaceApple() {
+        if (!appleAvailable) {
+            do {
+                appleX = MathUtils.random(Gdx.graphics.getWidth()
+                        / SNAKE_MOVEMENT - 1) * SNAKE_MOVEMENT;
+                appleY = MathUtils.random(Gdx.graphics.getHeight()
+                        / SNAKE_MOVEMENT - 1) * SNAKE_MOVEMENT;
+                appleAvailable = true;
+            } while (appleX == snakeX && appleY == snakeY);
+        }
     }
 
     private void checkForOutOfBounds() {
@@ -84,5 +126,17 @@ public class GameScreen extends ScreenAdapter {
                 break;
             }
         }
+    }
+
+    private void queryInput() {
+        boolean lPressed = Gdx.input.isKeyPressed(Input.Keys.LEFT);
+        boolean rPressed = Gdx.input.isKeyPressed(Input.Keys.RIGHT);
+        boolean uPressed = Gdx.input.isKeyPressed(Input.Keys.UP);
+        boolean dPressed = Gdx.input.isKeyPressed(Input.Keys.DOWN);
+
+        if (lPressed) snakeDirection = LEFT;
+        if (rPressed) snakeDirection = RIGHT;
+        if (uPressed) snakeDirection = UP;
+        if (dPressed) snakeDirection = DOWN;
     }
 }
